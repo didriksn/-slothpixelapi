90c1ede2d1c1783190dd309fdf25b62193dd04c1
didrik <didrik.sommersten@gmail.com>
2023-03-17 19:00:30 +0100

    updated to show amount sold and  in how many order

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Made with slothpixelapi
 
-Checks for the lowest sell price for Enchanted Acacia Logs in Hypixel Skyblock!
+Checks for the lowest sell price and the amount sold in how many orders for Enchanted Acacia Logs in Hypixel Skyblock!
 
 ## API info
 Check out https://docs.slothpixel.me/ for more information about the API
--- index.js
+++ index.js
@@ -4,5 +4,6 @@ request('https://api.slothpixel.me/api/skyblock/bazaar/ENCHANTED_ACACIA_LOG', { 
   if (err) { return console.log(err); }
 
   let lowestSellOfferThatShowsUpInTheSellSummaryObject = res["body"]["sell_summary"].length - 1; // -1 because array indexes start at 0
-  console.log(res["body"]["sell_summary"][lowestSellOfferThatShowsUpInTheSellSummaryObject]["pricePerUnit"]);
+
+  console.log(res["body"]["sell_summary"].slice(lowestSellOfferThatShowsUpInTheSellSummaryObject - 5, lowestSellOfferThatShowsUpInTheSellSummaryObject));
 });
