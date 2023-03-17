25ae91d470c54d0be6593ecdf737d5030f6544c1
didrik <didrik.sommersten@gmail.com>
2023-03-17 17:27:24 +0100

    image.png

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
-const lowestSellOfferThatShowsUpInTheSellSummaryObject = 29;
-
 request('https://api.slothpixel.me/api/skyblock/bazaar/ENCHANTED_ACACIA_LOG', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
+
+  let lowestSellOfferThatShowsUpInTheSellSummaryObject = res["body"]["sell_summary"].length - 1; // -1 because array indexes start at 0
   console.log(res["body"]["sell_summary"][lowestSellOfferThatShowsUpInTheSellSummaryObject]["pricePerUnit"]);
 });
+
+//stff
