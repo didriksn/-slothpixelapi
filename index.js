0443296fe021920244f0b9de086eb8726f406d09
didrik <didrik.sommersten@gmail.com>
2023-03-17 22:09:51 +0100

    no longer shows orders and amount, but shows only the price of the five cheapest.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 request('https://api.slothpixel.me/api/skyblock/bazaar/ENCHANTED_ACACIA_LOG', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   let lowestSellOfferThatShowsUpInTheSellSummaryObject = res["body"]["sell_summary"].length - 1; // -1 because array indexes start at 0
+  let arr = res["body"]["sell_summary"].slice(lowestSellOfferThatShowsUpInTheSellSummaryObject - 5, lowestSellOfferThatShowsUpInTheSellSummaryObject);
 
-  console.log(res["body"]["sell_summary"].slice(lowestSellOfferThatShowsUpInTheSellSummaryObject - 5, lowestSellOfferThatShowsUpInTheSellSummaryObject));
-  
+  arr.forEach(i => console.log(i["pricePerUnit"]));
 });
